d02478e8b69fa1039acacb86c86173698deb5959
kasankal <106431952+Kasankal@users.noreply.github.com>
2022-09-13 11:25:25 +0300

    heading half complete

--- pages/index.js
+++ pages/index.js
@@ -42,13 +42,48 @@ export default function Home() {
         </nav>
       </section>
       
-      <section className=''>
-      <Image
-      src= "/drone2.jpg"
-      height = {1080}
-      width = {2160}
-      alt = "homepage image"
-        />
+      <section className='flex relative '>
+        <Image
+        className='flex absolute'
+        src= "/drone2.jpg"
+        height = {720}
+        width = {2160}
+        alt = "homepage image"
+          />
+        <section className='flex flex-col absolute text-white justify-around  w-full h-full '>
+          <div className='flex  flex-row mt-10 mx-20 justify-between'>
+            <div>Site of the Day    September 13,2022</div>
+            <div className='fill-white '>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+              </svg>
+
+            </div>
+          </div>
+          <div className='mx-auto'>
+            <section className='text-center'>Henry Northington</section>
+            <section className='text-sm'>BY MASON PAJUNAS FROM UNITED STATES WITH 7.35</section>
+            <section className='flex flex-row'>
+              <div className='flex flex-col relative'>
+                <span></span>
+                <span></span>
+                <span className='text-xs'>DESIGN</span>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </section>
+          </div>
+          <div className='flex flex-row justify-between mx-20'>
+            <div className='flex flex-row'>
+              <div className='rounded-full bg-blue-400 text-sm px-2   border border-white/0'>Visit Site</div>
+              <div className='rounded-full text-sm px-3 ml-5 border-white border  '>Collect</div>
+            </div>
+            <div>
+              <div className='rounded-full text-sm border border-white px-2'>Jury Votes</div>
+            </div>
+          </div>
+        </section>
       </section>
       <section>
         <section className='mx-20 my-10'>
@@ -303,9 +338,48 @@ export default function Home() {
         </section>
       </section>
 
-      <footer className='divide-y divide-gray-200'>
-        <section></section>
-        <section></section>
+      <footer className='flex flex-col divide-y divide-gray-200 bg-gray-900 text-white mt-40 font-mono '>
+        <section className='mx-20 my-5'>
+          <div className='my-5 flex flex-row justify-between'>
+            <div className=''>The awards for design, creativity and innovation on the Internet.</div>
+            <div className=''>Next Conferences  Amsterdam and Tokyo</div>
+          </div>
+          <div className='my-5'>
+            <ul className='flex flex-row text-sm '>
+              <li className='mr-3'>Web Design Courses</li>
+              <li className='mr-3'>Directory</li>
+              <li className='mr-3'>Jobs</li>
+              <li className='mr-3'>About Us</li>
+              <li className='mr-3'>Winners</li>
+              <li className='mr-3'>Nominees</li>
+              <li className='mr-3'>Blog</li>
+            </ul>
+          </div>
+          <div>
+            <ul className='flex flex-row'>
+              <li className='mr-3 font-sans mb-20'>Follow Us</li>
+              <li className='mr-3'>Twitter</li>
+              <li className='mr-3'>Facebook</li>
+              <li className='mr-3'>Youtube</li>
+              <li className='mr-3'>Instagram</li>
+              <li className='mr-3'>LinkedIn</li>
+              <li className='mr-3'>Pinterest</li>
+              <li className='mr-3'>Reddit</li>
+              <li className='mr-3'>WhatsApp</li>
+            </ul>
+          </div>
+        </section>
+        <section className='mx-20 text-sm'>
+          <ul className='my-5 flex flex-row'>
+            <li className='mr-3'>Contact Us</li>
+            <li className='mr-3'>F.A.Q</li>
+            <li className='mr-3'>Cookies Policy</li>
+            <li className='mr-3'>Legal Terms</li>
+            <li className='mr-3'>Privacy Policy</li>
+            <li className='mr-3'>English</li>
+            <li className='mr-3'>Private Cloud Stackscale</li>
+          </ul>
+        </section>
       </footer>
     </div>
   )
