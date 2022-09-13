c0e77a01dbbf177e5b9c28a7541e7ab856242eb9
kasankal <106431952+Kasankal@users.noreply.github.com>
2022-09-13 11:59:02 +0300

    debugging deploy

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
           <div className='flex  flex-row mt-10 mx-20 justify-between'>
             <div>Site of the Day    September 13,2022</div>
             <div className='fill-white '>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
               </svg>
 
